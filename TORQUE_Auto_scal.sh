bdbacbe3c185fa6687ff1a336d5ae4fdcb74b51b
Ankit Tyagi <50823933+ankit291@users.noreply.github.com>
2019-11-07 12:19:28 +0530

    main code

--- TORQUE_Auto_scal.sh
+++ TORQUE_Auto_scal.sh
@@ -2,16 +2,13 @@
 
 rm -f OFFLINE_NODES DOWN_NODES FREE_RUNNING_NODES CURR_NODES_WIT_CNT nod_det nod_cnt_available null_nodes to_scale_dwn_node aFile_block_Job_tim YES_OR_NOT NOT_RUNNING_MIN_NODE RUNNING_NODES_TYPE sort_nod_det node_types  ###deleting existed files , which will create every time when this script going to execute
 
-. /root/ANKIT/Scale.properties
+. /root/user.name/Scale.properties
 
-echo "First we are goint to define variables , which will not required to change in entire process .
-		"
+echo "First we are goint to define variables , which will not required to change in entire process."
 ### This below query used to get total types of nodes in our environment.
 pbsnodes | grep -B 3 "state = " | grep -v "power_state\|\-\-\|mom_" | grep -v '^$' | awk 'NR%2{printf "%s ",$0;next;}1'| awk '{print $1}' | awk -F"-" '{print $1}' > node_types
 
 ##Below command get average Gcurrent jobs per node.
-
-
 AVG_JOBS_PER_NODE=`echo $(($(qstat -q | tail -1 | grep -Eo '[[:digit:]]{1,8}' | head -1)/$(pbsnodes -l free | grep -v torquectrl | wc -l)))`
 
 ##Below command is used to get current total jobs on all nodes
@@ -24,37 +21,31 @@ INDX_NODE=`echo ${#Node_Type[@]}`
 QUEUE_JOB=`/opt/moab/bin/showq | grep " blocked jobs" | awk '{print $1}'`
 
 
-Job_Per_Node=`qstat -n -e  | grep torque...- | sort  | uniq -c`   ###############
+Job_Per_Node=`qstat -n -e  | grep torque...- | sort  | uniq -c`   
 
 ## This is used to get queue count on our important queues.
 for i in `qstat |awk 'length($2) == 16 { print $1"   "$2 }' | grep SC | awk -F"." '{print $1}' |grep -v "\[\|-" | head`; do tracejob $i | grep "exec_host=" | awk -F"=" '{print $10}'| awk '{print $1}' >> SC_jobs; done
 if [ -s SC_jobs ];
-then
-SC_job_cnt=`cat SC_jobs | wc -l`
-echo `cat SC_jobs` | sed -e 's/\n//g' | sed 's/ /\|/g' >SC_Jobs_refr
-var_SC_job=`cat SC_Jobs_refr`
+    then
+    SC_job_cnt=`cat SC_jobs | wc -l`
+    echo `cat SC_jobs` | sed -e 's/\n//g' | sed 's/ /\|/g' >SC_Jobs_refr
+    var_SC_job=`cat SC_Jobs_refr`
 else
-var_SC_job='NOTHING_HERE'
+    var_SC_job='NOTHING_HERE'
 fi
 #queue get to compare usermart count
-
 USERMART_Q_cnt=`qstat -q | grep -i USERMARTQ | awk 'BEGIN {sum=0} ; {sum+=$7} END {print sum}'`
-
 CUSTOMQ_Q_cnt=`qstat -q | grep -i CUSTOMQ | awk 'BEGIN {sum=0} ; {sum+=$7} END {print sum}'`
-
 WORKBENCHQ_Q_cnt=`qstat -q | grep -i WORKBENCHQ | awk 'BEGIN {sum=0} ; {sum+=$7} END {print sum}'`
-
 PIPELINEq=`qstat -q | grep -i pipelineQ | awk 'BEGIN {sum=0} ; {sum+=$7} END {print sum}'`
 
-echo "There is total $INDX_NODE types of nodes are available in out Torque environment(except torquectr1).
-	"
+echo "There is total $INDX_NODE types of nodes are available in out Torque environment(except torquectr1).	"
 
 ######### Below commands are getiind details of nodes state , running , down or offline
 
 pbsnodes | grep -B 3 "state = " | grep -v "power_state\|\-\-\|mom_" | grep -v '^$' | awk 'NR%2{printf "%s ",$0;next;}1' |grep -v torquectrl | grep offline | grep -v down | awk '{print $1}' > OFFLINE_NODES
 pbsnodes | grep -B 3 "state = " | grep -v "power_state\|\-\-\|mom_" | grep -v '^$' | awk 'NR%2{printf "%s ",$0;next;}1'| grep -v torquectrl |grep -i down | awk '{print $1}' > DOWN_NODES
 pbsnodes | grep -B 3 "state = " | grep -v "power_state\|\-\-\|mom_" | grep -v '^$' | awk 'NR%2{printf "%s ",$0;next;}1'| grep -v torquectrl |grep -i free | awk '{print $1}' > FREE_RUNNING_NODES
-
 pbsnodes | grep -B 3 "state = " | grep -v "power_state\|\-\-\|mom_" | grep -v '^$' | awk 'NR%2{printf "%s ",$0;next;}1'| grep -v torquectrl |grep -i free | awk '{print $1}'| sort | uniq -c >CURR_NODES_WIT_CNT
 
 offline_cnt=`cat OFFLINE_NODES | wc -l`
@@ -93,8 +84,8 @@ then
 
 				echo "nodes $Off_line_to_UP is available to change from offline state Running."
 				echo "We are facing load increased in out torque environment and we have currently some nodes in offline state , So we are going to change their state to online."
-				Instance_ID=`grep $Off_line_to_UP /root/ANKIT/Node_details | awk '{print $2}'`
-				rregion=`grep $Off_line_to_UP /root/ANKIT/Node_details | awk '{print $3}'`
+				Instance_ID=`grep $Off_line_to_UP /root/user.name/Node_details | awk '{print $2}'`
+				rregion=`grep $Off_line_to_UP /root/user.name/Node_details | awk '{print $3}'`
 				pbsnodes -c  $Off_line_to_UP
 cat > outchk <<EOL
 Hi Team,
@@ -102,10 +93,9 @@ Hi Team,
 We Just change state from offline to online of Torque node $Off_line_to_UP .
 
 Regards
-Ankit Tyagi
-3Pillar Global
+User Name
 EOL
-mail -s "Torque Node State chng to IN" user.name@gmail.com < outchk
+mail -s "Torque Node State chng to IN" user.name@g.gmailom < outchk
 
 #################################	SCALE UP ######################
 elif  [ $cntt -lt $node_cnt_for_up ];
@@ -124,8 +114,8 @@ egrep -v `echo $in_var_onlin_nod` AA_1 > AA_2
                                 node_going_scale=`cat AA_2 | head -1`
 
 				echo "Now we are goint to scale up to node $node_going_scale . "
-				instance_ID=`grep $node_going_scale /root/ANKIT/Node_details | awk '{print $2}'`
-				Region=`grep $node_going_scale  /root/ANKIT/Node_details | awk '{print $3}'`
+				instance_ID=`grep $node_going_scale /root/user.name/Node_details | awk '{print $2}'`
+				Region=`grep $node_going_scale  /root/user.name/Node_details | awk '{print $3}'`
 				aws ec2 start-instances --instance-ids $instance_ID --region $Region
 				sleep 180
 				echo "$node_going_scale has been started with instance ID $instance_ID"
@@ -138,8 +128,7 @@ Hi Team,
 We Just Scale up the Torque node $node_going_scale .
 
 Regards
-Ankit Tyagi
-3Pillar Global
+User Name
 EOL
 
 mail -s "Torque Node Scale UP detail" user.name@gmail.com < outchk
@@ -168,8 +157,8 @@ elif [[ $AVG_JOBS_PER_NODE -le $MINIMUM_AVG_JOBS_PER_NODE && $CURRENT_TOTAL_JOBS
 		NODE_TO_DOWN=`cat LEFT_TO_DWN | grep -v -i $IGNORE_NODES | awk -F"-" '{print $1}' | sort | uniq -c | awk '{if($1> $MAX_NOD_CNT_TO_SCAL_DWN) print $2}' | head -1`
 		NODE_NAME=`grep $NODE_TO_DOWN FREE_RUNNING_NODES | head -1`
 		echo "We are going to scale down node name \"$NODE_NAME\""
-		Instance_ID=`grep $NODE_NAME /root/ANKIT/Node_details | awk '{print $2}'`
-		rregion=`grep $NODE_NAME /root/ANKIT/Node_details | awk '{print $3}'`
+		Instance_ID=`grep $NODE_NAME /root/user.name/Node_details | awk '{print $2}'`
+		rregion=`grep $NODE_NAME /root/user.name/Node_details | awk '{print $3}'`
 		pbsnodes -o $NODE_NAME
 cat > outchk <<EOL
 Hi Team,
@@ -177,8 +166,7 @@ Hi Team,
 We Just Scale down the Torque node $NODE_NAME , and change state from online to offline  !!
 
 Regards
-Ankit Tyagi
-3Pillar Global
+user.name
 EOL
 
 		mail -s "Torque Node State Chng Online to Offline" user.name@gmail.com < outchk
